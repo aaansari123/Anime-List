e9cf2f6a2d9506c2df5a3786775dd3fb779d3724
aaansari123 <asadali1029@gmail.com>
2022-08-02 13:03:04 -0400

    saving before integrating teammate work

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,16 +30,29 @@ function to render quote and link
 
 // api call to get the quotes data
 function getAnimeQuotes(name){
-    var apiURL = 'https://animechan.vercel.app/api/quotes/anime?title=' + name;
+    var array = name.split(" ");
+    console.log(array);
+    var apiURL = 'https://animechan.vercel.app/api/quotes/anime?title=';
+    for (var i =0; i< array.length; i++){
+        apiURL += array[i];
+        if (i<array.length-1){
+            apiURL += "&";
+        }
+    }
+    console.log(apiURL);
+    
     fetch(apiURL).then(function (response){
         if (response.ok){
             response.json().then(function(data){
-                console.log(data);
-                console.log(data.length);   
+                // console.log(data);
+                // console.log(data.length);   
                 var randNum = Math.floor(Math.random() * data.length);
                 console.log(data[randNum].character);
                 console.log(data[randNum].quote);
             });
+        }
+        else{
+            console.log("please enter a valid anime name");
         }
 
     });
@@ -49,19 +62,33 @@ function getAnimeQuotes(name){
 //api call to get jikan data
 
 function getAnimeInfo(name){
+    var anime = name;
+    var indexUsed;
     var apiURL = 'https://api.jikan.moe/v4/anime?q=' + name;
     fetch(apiURL).then(function (response){
         if (response.ok){
             response.json().then(function(data){
                 console.log(data);
                 for( var i = 0; i < data.data.length; i++){
-                    console.log(data.data[i].title);
+                    // console.log(data.data[i].title);
+                    // console.log(name);
+                    var title = data.data[i].title.replace(':', "");
+                    // console.log(title.toLowerCase());
+
+                    if (title.toLowerCase() == name.toLowerCase()){
+                        console.log(i);
+                        indexUsed = i;
+                    }
+                    else{
+                        return;
+                    }
                 }
+                console.log(data.data[indexUsed].title);
+                console.log(data.data[indexUsed].url);
+                console.log(data.data[indexUsed].duration);
+                console.log(data.data[indexUsed].year);
+                console.log(indexUsed);
                 
-                // console.log(data.length);   
-                // var randNum = Math.floor(Math.random() * data.length);
-                // console.log(data[randNum].character);
-                // console.log(data[randNum].quote);
             });
         }
 
@@ -74,7 +101,6 @@ function getAnimeInfo(name){
 function handleSubmit(event){
     event.preventDefault();
     var animeName = userInput.value.trim();
-    console.log(animeName);
     getAnimeQuotes(animeName);
     getAnimeInfo(animeName);
 }
