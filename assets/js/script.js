5eeeab6a91e298bbb8dd69363c6262711c833f8b
aaansari123 <asadali1029@gmail.com>
2022-08-02 10:32:44 -0400

    Saving before integrating teammate's work

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+// insert variables for page elements here
+/*var submitButton
+area for quote
+area for link
+page element for list
+save to list button
+move to watched button
+page element for list of watched
+
+*/
+
+/* 
+global variables needed
+array for local storage of list to watch
+array for local storage of list of watched
+
+*/
+
+
+/* functions needed
+function to handle submit event
+function to handle save to list click event
+function to handle move to watched click event
+function to fetch api for anime quotes
+function to fetch api for myanimelist link
+function to render quote and link
+
+*/
+
+// api call to get the quotes data
+function getAnimeQuotes(){
+    var apiURL = 'https://animechan.vercel.app/api/quotes/anime?title=naruto'
+    fetch(apiURL).then(function (response){
+        if (response.ok){
+            response.json().then(function(data){
+                console.log(data);
+                console.log(data.length);   
+                var randNum = Math.floor(Math.random() * data.length);
+            });
+        }
+
+    });
+    
+}
+getAnimeQuotes();
+
